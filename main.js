b510619a29239e2d24afe9a92a957f426872b73a
Arniedude5342 <85857848+Arniedude5342@users.noreply.github.com>
2021-11-21 19:42:14 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var canvas = new fabric.Canvas('myCanvas');
 
  function new_image()
  {
-     fabric.Image.fromURL('birthdayimage.jpg', function(Img) {
+     fabric.Image.fromURL('birthdayimage1.jpg', function(Img) {
      block_image_object = Img;
  
      block_image_object.scaleToWidth(700);
